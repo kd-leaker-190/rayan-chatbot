b3f15e876eb2451583a568955cc41e203ae1410b
KD Leaker <shakibzeidi0@gmail.com>
2026-09-02 11:43:24 +0330

    visitors table

---
+++ backend/app/Models/Visitor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable([
+    'first_name',
+    'last_name',
+    'email',
+])]
+class Visitor extends Model
+{
+    //
+}
